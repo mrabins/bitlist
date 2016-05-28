c4d0d0095014fbf0529820a742d12b20cec22624
Mark Rabins <markrabins@gmail.com>
2016-05-28 07:23:50 -0700

    Removed and readded Root View Controller

--- TodosViewController.swift
+++ TodosViewController.swift
@@ -9,11 +9,20 @@
 import UIKit
 
 class TodosViewController: UIViewController {
+    
+    @IBOutlet weak var editButton: UIBarButtonItem!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        editButton.title = "Edit"
+        view.backgroundColor = UIColor.lightGrayColor()
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +30,4 @@ class TodosViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
