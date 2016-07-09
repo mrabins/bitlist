ce924f746dbfa93d23b3e65e26acf5048f0de63c
Mark Rabins <markrabins@gmail.com>
2016-07-09 00:05:21 -0700

    Set up reminders view

--- Bitlist/TodoViewController.swift
+++ Bitlist/TodoViewController.swift
@@ -13,11 +13,12 @@ class TodoViewController: UIViewController {
     @IBOutlet weak var completeButton: UIBarButtonItem!
     @IBOutlet weak var favoriteButton: UIBarButtonItem!
     @IBOutlet weak var deleteButton: UIBarButtonItem!
+    @IBOutlet weak var tableView: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
         
         completeButton.title = "Complete"
@@ -27,23 +28,51 @@ class TodoViewController: UIViewController {
         favoriteButton.tintColor = UIColor.blackColor()
         
         deleteButton.title = "Delete"
-
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func completeButtonPressed(sender: UIBarButtonItem) {
+        
     }
-    */
-
+    
+    @IBAction func favoriteButtonPressed (sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func deleteButtonPressed(sender: UIBarButtonItem) {
+        
+    }
+    
+    extension TodoViewController: UITableViewDelegate {
+        
+    }
+    
+    extension TodoViewController: UITableViewDataSource {
+        func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+            return UITableViewCell()
+        }
+        
+        func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return 0
+        }
+        
+        func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+            return 3
+        }
+    }
+    
+    
 }
