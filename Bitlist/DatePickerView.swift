0628aa202a7dabbf57b4b9667ccc790a799409be
Mark Rabins <mark.rabins@ensighten.com>
2016-08-09 21:48:17 -0700

    Set up datePickerView class and UI attributes

---
+++ Bitlist/DatePickerView.swift
@@ -0,0 +1,29 @@
+//
+//  DatePickerView.swift
+//  Bitlist
+//
+//  Created by Mark Rabins on 8/9/16.
+//  Copyright © 2016 self.edu. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerView: UIView {
+    
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
+    @IBAction func removeBarButtonItemPressed(sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func doneBarButtonItemPressed(sender: UIBarButtonItem) {
+        
+    }
+    
+    @IBAction func datePickerChanged(sender: UIDatePicker) {
+        
+    }
+
+
+}
